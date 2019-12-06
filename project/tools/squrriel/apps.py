f27e65e1c04821b1f46156f0ff1ab0857ef772fe
Shijia Zhang <sz2868@colubmai.edu>
2019-12-06 16:27:00 -0500

    initialize

---
+++ project/tools/squrriel/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SqurrielConfig(AppConfig):
+    name = 'squrriel'
---
+++ project/tools/squrriel/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+from django.utils.translation import gettext as_
+
+class Pet(models.Model):
+
